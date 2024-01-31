bec779e0cdc71008d22a9ff4c643be0aefe7395b
ualinisum <uali@nisum.com>
2024-01-31 15:03:24 +0500

    Updated assignment

--- assignment6/src/routes/book.ts
+++ assignment6/src/routes/book.ts
@@ -1,26 +1,11 @@
 import { Router } from "express";
 import book from "../controller/book";
-import shoppingCart from "../controller/shoppingCart";
-import order from "../controller/order";
 
-const router = Router();
+const bookRouter = Router();
 
-// Book CRUD
-router.get("/books", book.getAllBooks);
-router.post("/book", book.addBook);
-router.put("/book/:id", book.updateBook);
-router.delete("/book/:id", book.deleteBook);
+bookRouter.get("/books", book.getAllBooks);
+bookRouter.post("/book", book.addBook);
+bookRouter.put("/book/:id", book.updateBook);
+bookRouter.delete("/book/:id", book.deleteBook);
 
-// Add book to shopping cart
-router.post('/cart/add/:userId/:bookId', shoppingCart.addToShoppingCart);
-
-// Remove book from shopping cart
-router.delete('/cart/remove/:userId/:bookId', shoppingCart.removeFromShoppingCart);
-
-// Place an order
-router.post('/order/place/:userId', order.placeOrder);
-
-// Retrieve order history for a user
-router.get('/order/history/:userId', order.getOrderHistory);
-
-export default router;
+export default bookRouter;
